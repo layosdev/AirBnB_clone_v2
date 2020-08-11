faa0bd15e52e8092272f58d6aec7c9073be72d62
Felipe Layos <jfelipelayos@gmail.com>
2020-08-11 22:01:18 +0000

    Console tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,8 +7,10 @@ from uuid import UUID
 import json
 import os
 import pep8
+import console
 from console import HBNBCommand
-
+from io import StringIO
+from unittest.mock import patch
 
 class test_console(unittest.TestCase):
     """ test for console.py"""
@@ -18,11 +20,16 @@ class test_console(unittest.TestCase):
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(["console.py"])
         self.assertEqual(p.total_errors, 0)
-    
+
+    def test_docstrings(self):
+        """Check for docstrings."""
+        self.assertIsNotNone(HBNBCommand.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+
     def test_class_existence(self):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("hello command")
-            self.assertEqual("** class doesn't exist **\n", f.getvalue())
+            self.assertEqual("*** Unknown syntax: hello command\n", f.getvalue())
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create")
             self.assertEqual("** class name missing **\n", f.getvalue())
