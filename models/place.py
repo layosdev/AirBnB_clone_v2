b23d23774cdcbc81420b158e6d03af759372f1df
Felipe Layos <jfelipelayos@gmail.com>
2020-08-13 16:11:57 +0000

    fix

--- models/place.py
+++ models/place.py
@@ -2,38 +2,20 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey, Column, String, Integer, Float
-import os
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
 
     __tablename__ = "places"
-    if (os.getenv("HBNB_TYPE_STORAGE") == "db"):
-
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        description = Column(String(1024), nullable=True)
-        amenity_ids = []
-        reviews = relationship(
-            'Review', cascade='all, delete', backref='place')
-
-    else:
-
-        @property
-        def reviews(self):
-            """ get reviews """
-            review_list = []
-            for key, val in models.storage.items().all():
-                class_name = val.__class__.__name__
-                if class_name == 'Review':
-                    if val.place_id == self.id:
-                        review_list.append(val)
-            return (review_list)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    description = Column(String(1024), nullable=True)
+    amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -3,24 +3,13 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import ForeignKey, Column, String
 from sqlalchemy.orm import relationship
-import os
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
     __tablename__ = 'users'
-    if (os.getenv("HBNB_TYPE_STORAGE") == "db"):
-
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user', cascade='all, delete')
-        reviews = relationship('Review', backref='user', cascade='all, delete')
-
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref='user', cascade="all, delete")
