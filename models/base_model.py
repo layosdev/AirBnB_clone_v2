94dffdfce8adc55691529c599d616e76a34e1d70
Felipe Layos <jfelipelayos@gmail.com>
2020-08-14 02:43:12 +0000

    Fix date

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ import os
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
 
@@ -24,27 +25,27 @@ class BaseModel:
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
             if 'updated_at' in kwargs:
                 self.updated_at = datetime.strptime(kwargs['updated_at'],
                                                     '%Y-%m-%dT%H:%M:%S.%f')
             else:
-                self.updated_at = datetime.utcnow()
+                self.updated_at = datetime.now()
 
             if 'created_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs['created_at'],
+                self.created_at = datetime.strptime(kwargs['created_at'],
                                                     '%Y-%m-%dT%H:%M:%S.%f')
             else:
-                self.created_at = datetime.utcnow()
+                self.created_at = datetime.now()
             if '__class__' in kwargs:
                 del kwargs['__class__']
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
             for k, v in kwargs.items():
                 setattr(self, k, v)
-            """ self.__dict__.update(kwargs) """
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
