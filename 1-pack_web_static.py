6b69e5ceff50fcd373b5cdddad1ae8f1fd9e2842
Felipe Layos <jfelipelayos@gmail.com>
2020-08-17 18:39:16 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import time
 from fabric.api import *
+from datetime import datetime
 
 
 def do_pack():
     """ generates a .tgz archive from the contents of the web_static """
     local("sudo mkdir -p versions")
-    date = time.localtime().strftime("%Y%m%d%H%M%S")
-    file = ("versions/web_static_{}.tgz").format(date)
-    local("sudo tar -cvzf {} web_static".format(file))
-    return file
+    d = datetime.now().strftime("%Y%m%d%H%M%S")
+    f = ("versions/web_static_{}.tgz").format(d)
+    local("sudo tar -cvzf {} web_static".format(f))
+    return f
